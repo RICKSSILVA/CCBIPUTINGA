7d28008275872cf774e369bf2b87e87ba3c411a0
RICKSSILVA <39918096+RICKSSILVA@users.noreply.github.com>
2020-05-19 16:51:52 -0300

    ADICIONAR A FORMATAÇÃO ID NO HTML

---
+++ departamento.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Página Departamento</title>
+	<meta charset="utf-8">
+
+
+</head>
+<body>
+
+
+
+
+<?php
+
+	$Departamento = $_Post[ADM];
+
+	echo $Departamento;
+
+
+?>
+
+</body>
+</html>
